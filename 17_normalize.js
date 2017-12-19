c0ba2d1692cf1b59572718f7e3dbe89f6cae7a45
dangxiaoli <dangxiaoli@meituan.com>
2017-12-19 15:34:00 +0800

    add

--- 17_normalize.js
+++ 17_normalize.js
@@ -2,7 +2,7 @@
 * @Author: dangxiaoli
 * @Date:   2017-12-19 13:15:26
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2017-12-19 13:18:11
+* @Last Modified time: 2017-12-19 15:24:29
 */
 const {normalize} = require('path');
 
@@ -12,3 +12,4 @@ const {normalize} = require('path');
 console.log(normalize('/usr//local/bin/'))
 
 console.log(normalize('/usr//local/../bin/'))
+
--- 19_resolve.js
+++ 19_resolve.js
@@ -2,8 +2,10 @@
 * @Author: dangxiaoli
 * @Date:   2017-12-19 13:20:47
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2017-12-19 13:21:22
+* @Last Modified time: 2017-12-19 14:46:12
 */
 const {resolve} = require('path');
 
 console.log(resolve('./'))
+
+console.log(resolve('/foo/bar', './baz'))
