c63b2695d9e99a55292d68d6b315bef1e2c392b4
dangxiaoli <dangxiaoli@meituan.com>
2017-12-27 18:13:45 +0800

    Add

--- 23_paths.js
+++ 23_paths.js
@@ -2,7 +2,7 @@
 * @Author: dangxiaoli
 * @Date:   2017-12-19 13:47:57
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2017-12-20 13:38:27
+* @Last Modified time: 2017-12-26 13:50:48
 */
 const path = require('path')
 const mod = require('./02_cusmod.js');
@@ -15,3 +15,4 @@ console.log('./',path.resolve('./'))
 
 
 
+console.log(path.join(process.cwd(), 'a/b/a/'))
--- 41_readstream.js
+++ 41_readstream.js
@@ -2,12 +2,14 @@
 * @Author: dangxiaoli
 * @Date:   2017-12-24 21:24:40
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2017-12-24 21:40:59
+* @Last Modified time: 2017-12-26 14:21:11
 */
 
 
 const fs = require('fs');
 const rs = fs.createReadStream('./41_readstream.js');
 
+console.log(rs)
 
 rs.pipe(process.stdout);
+console.log(rs.pipe(process.stdout))
